e132feeb861903f3728acb9b0958c46b21017c7b
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-01-29 11:08:16 +0200

    import-fix

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,8 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const images = [
   {
     preview:
@@ -76,11 +81,6 @@ const markup = images
   .join('');
 
 document.querySelector('.gallery').innerHTML = markup;
-
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 var lightbox = new SimpleLightbox('.gallery a', {
   captionPosition: 'bottom',
